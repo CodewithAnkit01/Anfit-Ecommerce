a9c33f51b92e3d6e89357337b9db905ca0414ab1
Ankit Acharya <161122376+CodewithAnkit01@users.noreply.github.com>
2026-07-24 13:50:40 +0545

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"https://anfit-ecommerce.onrender.com",
+  baseURL:"https://anfit-ecommerce.onrender.com/api",
 });
 
 // Request interceptor (attach token)
