c1f49ac6044e4f3bba6afe0a236d84c8e8bdd17f
Ankit Acharya <ankitacharya1217@gmail.com>
2026-07-27 11:04:45 +0545

    an

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"https://anfitbackend.onrender.com",
+  baseURL:"https://anfitbackend.onrender.com/api/",
 });
 
 // Request interceptor (attach token)
