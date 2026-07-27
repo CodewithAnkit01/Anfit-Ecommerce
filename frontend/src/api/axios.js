a5ee691ff273619ce69d65e58a3f37f6220fe6f7
Ankit Acharya <161122376+CodewithAnkit01@users.noreply.github.com>
2026-07-27 07:19:46 +0545

    Fix baseURL casing in axios configura

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"https://anfitbackend.onrender.com/v1",
+  baseURL:"https://anfitbackend.onrender.com/V1",
 });
 
 // Request interceptor (attach token)
